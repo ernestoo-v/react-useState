905218b06bcd32a9915394ff9268bfcbd49c0e5e
Ernesto Villar <evillarperez@gmail.com>
2023-04-07 20:07:38 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  var [count, setCount] = useState(0);
+
+  function handleMinus() {
+    if (count > 0) {
+      setCount(prevCount => prevCount - 1)
+    }
+  }
+  function handleAdd() {
+    setCount(prevCount => prevCount + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={handleMinus}>-</button>
+      <p>{count}</p>
+      <button onClick={handleAdd}>+</button>
+    </>
+  )
 }
 
 export default App;
